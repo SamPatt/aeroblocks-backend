9903a1f8bb7652b5392452c41432c0a9dfe3f1e8
SamPatt <git@sampatt.com>
2024-02-22 14:25:47 -0500

    Adds CanvasState Model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,21 @@
-from mongoengine import Document, StringField
+from mongoengine import Document, StringField, ListField, EmbeddedDocument, EmbeddedDocumentField, DateTimeField
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+class CanvasState(EmbeddedDocument):
+    name = StringField(required=True)
+    data = StringField()  
+    created_at = DateTimeField(default=datetime.now)
+    updated_at = DateTimeField(default=datetime.now)
+    
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.now()
+        return super(CanvasState, self).save(*args, **kwargs)
 
 class User(Document):
     email = StringField(required=True, unique=True)
     password_hash = StringField(required=True)
+    canvases = ListField(EmbeddedDocumentField(CanvasState))
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
