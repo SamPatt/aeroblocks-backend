bd3d5f82afe969aa6d9b300cf90fe15239386f9f
SamPatt <git@sampatt.com>
2024-02-28 18:25:16 -0500

    Update URL

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/api/*": {"origins": "https://aeroblocks.netlify.app"}}, supports_credentials=True, allow_headers=["Content-Type", "Authorization"])
+CORS(app, resources={r"/api/*": {"origins": "https://aeroblocks.tech"}}, supports_credentials=True, allow_headers=["Content-Type", "Authorization"])
 
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
 jwt = JWTManager(app)
